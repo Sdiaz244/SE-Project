5886bfb92f6315de63dba6c86dd9b784770ae2ff
Steven <stevendiaz512@gmail.com>
2024-02-06 12:24:38 -0500

    new files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-red-800">
+        <p>
+           FBGM 
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App;
